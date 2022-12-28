90edc8f07f7e44f64f04acfcc081923bf7357d2a
Michał Masiulanis <m.masiulanis@gmail.com>
2022-12-28 12:44:19 +0100

    zad 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+// klucz do local storage
+// kapitaliki - zmienne, ktore maja sluzyc w calym pliku/aplikacji
+const STORAGE_KEY = 'videoplayer-current-time';
+
+function trackTime({ seconds } = 0) {
+  console.log({ seconds });
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+player.on('timeupdate', trackTime);
+
+player
+  .setCurrentTime(trackTime())
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+      default:
+        // some other error occurred
+        break;
+    }
+  });
